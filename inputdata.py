c4dd8a6d53b5b35b72b23d9fb24cefe38a3acfbf
rifqi rasyad <rifqirasyad129@gmail.com>
2024-11-19 22:36:34 +0700

    updated

---
+++ inputdata.py
@@ -0,0 +1,42 @@
+# Fungsi untuk menghitung nilai akhir
+def hitung_nilai_akhir(tugas, uts, uas):
+    return round((tugas * 0.3) + (uts * 0.3) + (uas * 0.4), 2)
+
+# Inisialisasi list untuk menampung data mahasiswa
+data_mahasiswa = []
+no = 1
+
+while True:
+    print("Nama :", end=" ")
+    nama = input()
+    print("NIM :", end=" ")
+    nim = input()
+    print("Nilai Tugas :", end=" ")
+    nilai_tugas = int(input())
+    print("Nilai UTS :", end=" ")
+    nilai_uts = int(input())
+    print("Nilai UAS :", end=" ")
+    nilai_uas = int(input())
+    
+    # Hitung nilai akhir
+    nilai_akhir = hitung_nilai_akhir(nilai_tugas, nilai_uts, nilai_uas)
+    
+    # Simpan data ke list
+    data_mahasiswa.append([no, nama, nim, nilai_tugas, nilai_uts, nilai_uas, nilai_akhir])
+    no += 1
+    
+    print("Tambah data(y/t)?", end=" ")
+    lanjut = input().lower()
+    if lanjut != 'y':
+        break
+
+# Cetak header tabel
+print("="*61)
+print("| No | Nama      | NIM   | Tugas | UTS | UAS | Akhir |")
+print("="*61)
+
+# Cetak setiap data mahasiswa
+for data in data_mahasiswa:
+    print(f"| {data[0]:<2} | {data[1]:<9} | {data[2]:<5} | {data[3]:<5} | {data[4]:<3} | {data[5]:<3} | {data[6]:<5} |")
+
+print("="*61)
